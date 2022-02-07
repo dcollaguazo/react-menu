41b729c81d9cbda1922d8ecbd79dda5424d6ff27
Daniela Collaguazo <dcct84@gmail.com>
2022-02-07 16:10:16 -0500

    Add items to cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React, { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <React.Fragment>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </React.Fragment>
   );
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,21 @@
 import React from "react";
+import mealsImage from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
-  return;
-  <React.Fragment>
-    <header>
-      <h1>ReactMeals</h1>
-    </header>
-    <button>Cart</button>
-    <div>
-      <img />
-    </div>
-  </React.Fragment>;
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Header;
