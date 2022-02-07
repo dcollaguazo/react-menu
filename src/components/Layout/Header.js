074d1afdb73264744b5af3b6f6351239db198a56
Daniela Collaguazo <dcct84@gmail.com>
2022-02-07 14:01:15 -0500

    project setup

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = (props) => {
+  return;
+  <React.Fragment>
+    <header>
+      <h1>ReactMeals</h1>
+    </header>
+    <button>Cart</button>
+    <div>
+      <img />
+    </div>
+  </React.Fragment>;
+};
+
+export default Header;
